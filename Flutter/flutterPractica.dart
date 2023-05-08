4b21b9c497c06e555843cd1dcb2d711f5cb4fda4
Anette Villalón <liuflee@outlook.com>
2023-05-08 00:17:55 -0400

    a

---
+++ Flutter/flutterPractica.dart
@@ -0,0 +1,22 @@
+void main() {
+  var uwu = "uwu";
+  var cont = 0;
+
+  for (var i = 0; i <= 100; i += 2) {
+    print(i);
+    if (i == 50) {
+      print(uwu);
+    } else if (i == 70) {
+      print("owo");
+    }
+  }
+
+  while (true) {
+    cont += 5;
+    print(cont);
+    if (cont == 70) {
+      break;
+    }
+  }
+}
+
