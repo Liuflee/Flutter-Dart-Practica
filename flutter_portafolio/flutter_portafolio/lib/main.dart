954c606b0d3649ec9b1567ad39f171cdb8df45a0
Anette Villalón <liuflee@outlook.com>
2023-06-12 23:21:23 -0400

    flutter new project

---
+++ flutter_portafolio/flutter_portafolio/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Portafolio());
+
+class Portafolio extends StatelessWidget {
+  const Portafolio({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Portafolio de Anette",
+      home: Inicio(),
+    );  
+  }
+}
+
+class Inicio extends StatefulWidget {
+  const Inicio({super.key});
+
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Portafolio de Anette"),
+      ),
+      body: Center(
+        child: Text("Contenido")
+        ),
+    );
+  }
+}
